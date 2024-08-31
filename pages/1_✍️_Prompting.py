15c43b839e315304940fcd67551a2ca4dbe821ec
Andre Kutianski <andre@eunarede.com>
2024-08-31 02:18:14 +0000

    chore: Refactor text_to_image page and remove unused code

--- pages/1_✍️_Prompting.py
+++ pages/1_✍️_Prompting.py
@@ -40,30 +40,30 @@ with st.form("text_to_image"):
             image_bytes = response.content
             st.download_button("Download Image", data=image_bytes, file_name="generated_image.png", mime="image/png")
             f"_Generated with [Cloudflare Workers AI](https://developer.cloudflare.com/workers-ai/) using the `{model}_`"
-        with st.spinner("Creating additional prompt suggestions..."):
-            prompt_model = "@hf/thebloke/mistral-7b-instruct-v0.1-awq"
-            prompt_url = f"https://api.cloudflare.com/client/v4/accounts/{account_id}/ai/run/{prompt_model}"
-            system_message = """
-            You are a Stable Diffusion prompt engineer. 
+        # with st.spinner("Creating additional prompt suggestions..."):
+        #     prompt_model = "@hf/thebloke/mistral-7b-instruct-v0.1-awq"
+        #     prompt_url = f"https://api.cloudflare.com/client/v4/accounts/{account_id}/ai/run/{prompt_model}"
+        #     system_message = """
+        #     You are a Stable Diffusion prompt engineer. 
             
-            The user is going to give you a prompt, and you will provide three detailed suggestions. 
+        #     The user is going to give you a prompt, and you will provide three detailed suggestions. 
             
-            Use various photo stylistic terms.
-            """
-            prompt_suggestion_response = requests.post(
-                prompt_url,
-                headers=headers,
-                json={
-                    "messages": [
-                        {"role": "system", "content": system_message},
-                        {"role": "user", "content": prompt},
-                    ]
-                },
-            )
-            json = prompt_suggestion_response.json()
-            result = json["result"]
-            if "response" in result:
-                st.write(result["response"])
-                f"_Generated with [Cloudflare Workers AI](https://developer.cloudflare.com/workers-ai/) using the `{prompt_model}` model_"
-            else:
-                st.write(result)
+        #     Use various photo stylistic terms.
+        #     """
+        #     prompt_suggestion_response = requests.post(
+        #         prompt_url,
+        #         headers=headers,
+        #         json={
+        #             "messages": [
+        #                 {"role": "system", "content": system_message},
+        #                 {"role": "user", "content": prompt},
+        #             ]
+        #         },
+        #     )
+        #     json = prompt_suggestion_response.json()
+        #     result = json["result"]
+        #     if "response" in result:
+        #         st.write(result["response"])
+        #         f"_Generated with [Cloudflare Workers AI](https://developer.cloudflare.com/workers-ai/) using the `{prompt_model}` model_"
+        #     else:
+        #         st.write(result)
